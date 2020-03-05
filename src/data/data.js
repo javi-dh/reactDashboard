7a99c3c7c93812104c68e37d98022536b7f48bc9
javi-dh <javier@digitalhouse.com>
2020-03-05 09:39:06 -0300

    2nd commit - Changes in data/data.js

--- src/data/data.js
+++ src/data/data.js
@@ -17,22 +17,4 @@ export default [
 		icon: "fa-user-check",
 		border: "border-left-warning",
 	},
-	{
-		title: "Products in Database",
-		value: 535,
-		icon: "fa-clipboard-list",
-		border: "border-left-primary",
-	},
-	{
-		title: "Amount in products",
-		value: 546456,
-		icon: "fa-dollar-sign",
-		border: "border-left-success",
-	},
-	{
-		title: "Users quantity",
-		value: 38,
-		icon: "fa-user-check",
-		border: "border-left-warning",
-	},
 ];
